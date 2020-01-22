330234bed22527084c888bdade89ea5d5e4e8380
Lucas MOULIN <lmoulin@e2r4p3.42.fr>
2020-01-23 00:04:17 +0100

    ex01

---
+++ Day02/ex01/main.py
@@ -0,0 +1,36 @@
+class ObjectC(object):
+    def __init__(self):
+        pass
+
+def doom_printer(obj):
+    if obj is None:
+        print("ERROR")
+        print("end")
+        return
+    for attr in dir(obj):
+        if attr[0] != '_':
+            value = getattr(obj, attr)
+            print("{}: {}".format(attr, value))
+    print("end")
+
+def what_are_the_vars(*args, **kwargs):
+    obj = ObjectC()
+    for var, value in kwargs.items():
+        setattr(obj, var, value)
+    for i in range(0, len(args)):
+        setattr(obj, "var_%d: " % i, args[i])
+    return (obj)
+    pass
+
+
+if __name__ == "__main__":
+    obj = what_are_the_vars(7)
+    doom_printer(obj)
+    obj = what_are_the_vars("ft_lol", "Hi")
+    doom_printer(obj)
+    obj = what_are_the_vars()
+    doom_printer(obj)
+    obj = what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world")
+    doom_printer(obj)
+    obj = what_are_the_vars(42, a=10, var_0="world")
+    doom_printer(obj)
